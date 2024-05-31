cb0980cdcdef9e4a165646ad0e332ae6a3119b9c
MantraGitH <camilo.arias56@hotmail.com>
2024-05-31 17:59:57 -0300

    07 Pilas y Colas + extra terminado (?)

--- 07-PILAS Y COLAS/javascript/extra.js
+++ 07-PILAS Y COLAS/javascript/extra.js
@@ -10,3 +10,56 @@ DIFICULTAD EXTRA (opcional):
 *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
 *   interpretan como nombres de documentos. 
 */
+
+//codigo a modo de ejemplo para entender el funcionamiento de stack y queue, no funciona como tal.
+
+function webNavigation() {
+    let stack = [];
+
+    while (true) {
+        let action = prompt("Añade una url o interactúa con palabras adelante/atrás/salir:");
+
+        if (action === "salir") {
+            console.log("Saliendo del navegador web.");
+            break;
+        } else if (action === "adelante") {
+            // No se hace nada en caso de 'adelante' porque no hay funcionalidad especificada
+        } else if (action === "atrás") {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            stack.push(action);
+        }
+
+        if (stack.length > 0) {
+            console.log(`Has navegado a la web: ${stack[stack.length - 1]}.`);
+        } else {
+            console.log("Estás en la página de inicio.");
+        }
+    }
+}
+
+webNavigation();
+
+function sharedPrinter() {
+    let queue = [];
+
+    while (true) {
+        let action = prompt("Añade un documento o selecciona imprimir/salir:");
+
+        if (action === "salir") {
+            break;
+        } else if (action === "imprimir") {
+            if (queue.length > 0) {
+                console.log(`Imprimiendo: ${queue.shift()}`);
+            }
+        } else {
+            queue.push(action);
+        }
+
+        console.log(`Cola de impresión: ${queue}`);
+    }
+}
+
+sharedPrinter();
