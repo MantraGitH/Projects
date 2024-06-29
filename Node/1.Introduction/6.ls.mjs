9adef9a6e615c7cd0379e62e78b97713476f407e
MantraGitH <camilo.arias56@hotmail.com>
2024-06-29 10:13:03 -0300

    node: 6.ls edit, 8.ls-advanced

--- Node/1.Introduction/6.ls.mjs
+++ Node/1.Introduction/6.ls.mjs
@@ -1,11 +1,35 @@
-import fs from "node:fs";
+import { readdir } from "node:fs/promises";
 
-fs.readdir(".", (err, files) => {
-  if (err) {
-    console.error("Error al leer el directorio: ", err);
-    return;
-  }
-  files.forEach((file) => {
-    console.log(file);
+readdir(".")
+  .then((files) => {
+    files.forEach((file) => {
+      console.log(file);
+    });
+  })
+  .catch((err) => {
+    if (err) {
+      console.error("Error al leer el directorio: ", err);
+      return;
+    }
   });
-});
+
+// node 6.ls.mjs
+
+//vvv
+
+// 1.os-info.mjs
+// 2.fs-stat.mjs
+// 3.fs-readFile-sync.mjs       
+// 3.fs-readFile.mjs
+// 4.fs-async-await-parallel.mjs
+// 4.fs-async-await.js
+// 4.fs-async-await.mjs
+// 4.fs-promises.mjs
+// 5.path.mjs
+// 6.ls.mjs
+// 7.process.mjs
+// 8.ls-advanced.mjs
+// cjs
+// etc.txt
+// etc2.txt
+// mjs
---
+++ Node/1.Introduction/8.ls-advanced.mjs
@@ -0,0 +1,72 @@
+// mejorando la app: listar archivos/directorios
+
+// Sistema de módulos
+import { readdir, stat } from "node:fs/promises";
+import { join } from "node:path";
+
+//process
+const folder = process.argv[2] ?? ".";
+
+//asincronía
+async function ls(folder) {
+  let files;
+  try {
+    //asincronia secuencial: hasta que no lea el directorio no continúa
+    files = await readdir(folder);
+  } catch {
+    console.error(red(` No se pudo leer el directorio ${folder}`));
+    process.exit(1);
+  }
+
+  //recuperando en paralelo la información de todos los ficheros del directiorio
+  const filesPromises = files.map(async (file) => {
+    const filePath = join(folder, file);
+    let stats;
+
+    try {
+      stats = await stat(filePath); // status - información del archivo
+    } catch {
+        //si hay un error 
+      console.error(`No se pudo leer el archivo ${filePath}`);
+      process.exit(1);
+    }
+
+    //filesystem no solo para leer el directiorio sino para recuperar la información de cada archivo
+    const isDirectory = stats.isDirectory();
+    // "d" = directorio, "f" = fichero
+    const fileType = isDirectory ? "d" : "f";
+    const fileSize = stats.size.toString();
+    const fileModified = stats.mtime.toLocaleString();
+    // utilizando la consola para enseñar cada archivo
+    return `${fileType} ${file.padEnd(20)} ${fileSize
+      .toString()
+      .padStart(10)} ${fileModified}`;
+  });
+
+  const filesInfo = await Promise.all(filesPromises);
+
+  filesInfo.forEach((fileInfo) => console.log(fileInfo));
+}
+
+ls(folder);
+
+// node 8.ls-advanced.mjs
+
+// vvv
+
+// f 1.os-info.mjs               484 8/6/2024, 3:05:45 p. m.
+//f 2.fs-stat.mjs               301 5/6/2024, 2:53:34 p. m.
+//f 3.fs-readFile-sync.mjs        507 19/6/2024, 1:02:45 p. m.       
+//f 3.fs-readFile.mjs           541 19/6/2024, 1:02:48 p. m.
+//f 4.fs-async-await-parallel.mjs        429 19/6/2024, 4:49:37 p. m.
+//f 4.fs-async-await.js        1105 19/6/2024, 4:51:15 p. m.
+//f 4.fs-async-await.mjs        566 19/6/2024, 4:49:15 p. m.
+//f 4.fs-promises.mjs           658 19/6/2024, 4:49:45 p. m.
+//f 5.path.mjs                  475 26/6/2024, 4:09:04 p. m.
+//f 6.ls.mjs                    281 29/6/2024, 9:47:02 a. m.
+//f 7.process.mjs               452 26/6/2024, 5:27:50 p. m.
+//f 8.ls-advanced.mjs          1144 29/6/2024, 9:58:05 a. m.
+//d cjs                           0 5/6/2024, 2:11:12 p. m.
+//f etc.txt                      11 5/6/2024, 2:50:54 p. m.
+//f etc2.txt                     74 5/6/2024, 2:58:19 p. m.
+//d mjs                           0 5/6/2024, 2:19:12 p. m.
